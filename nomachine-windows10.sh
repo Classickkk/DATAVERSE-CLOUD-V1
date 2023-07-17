0419d66c1d3286f016ad345c89faff2300e96eaf
Classickkk <123447874+Classickkk@users.noreply.github.com>
2023-07-17 06:58:36 -0300

    Update nomachine-windows10.sh

--- nomachine-windows10.sh
+++ nomachine-windows10.sh
@@ -1,86 +1,47 @@
-#!/bin/bash
-
 wget -O ng.sh https://raw.githubusercontent.com/Classickkk/DATAVERSE-CLOUD-V1/main/ngrok.sh > /dev/null 2>&1
 chmod +x ng.sh
 ./ng.sh
 
-function goto {
+
+function goto
+{
     label=$1
     cd 
-    cmd=$(sed -n "/^:[[:blank:]][[:blank:]]*${label}/{:a;n;p;ba};" $0 | grep -v ':$')
+    cmd=$(sed -n "/^:[[:blank:]][[:blank:]]*${label}/{:a;n;p;ba};" $0 | 
+          grep -v ':$')
     eval "$cmd"
     exit
 }
 
-# Defina o token do ngrok como padrão
-NGROK_TOKEN="2Se7QKvfXFYzwei9Q7PZYfArMlF_3idEDMzfXikrJfRWzk54J"
-
-# Configuração do ngrok
+: ngrok
 clear
-echo "CONFIGURANDO O NGROK..."
-./ng.sh authtoken $NGROK_TOKEN
+echo "CRIE SEU TOKEN AQUI: https://dashboard.ngrok.com/get-started/your-authtoken"
+read -p "COLE O AUTHTOKEN DO NGROK AQUI: " CRP
+./ngrok authtoken $CRP 
 
-# Configuração do servidor
 clear
-SERVER_REGION="sa" # Defina a região do servidor como padrão
-
-# Configurações disponíveis
-# us - ESTADOS UNIDOS (Ohio)
-# eu - EUROPA (Frankfurt)
-# ap - ASIA/PACÍFICO (Singapore)
-# au - AUSTRÁLIA (Sydney)
-# sa - AMÉRICA DO SUL (São Paulo)
-# jp - JAPÃO (Tokyo)
-# in - ÍNDIA (Mumbai)
-
-echo "CONFIGURAÇÃO DO SERVIDOR..."
-echo "REGIÃO ESCOLHIDA: $SERVER_REGION"
-sleep 2
-
-# Iniciando o ngrok em segundo plano
-echo "INICIANDO O NGROK..."
-./ng.sh tcp --region $SERVER_REGION 4000 &>/dev/null &
+echo "REPOSITÓRIO: https://github.com/Classickkk/DATAVERSE-CLOUD-V1/tree/main"
+echo "======================="
+echo "ESOLHA A REGIÃO (PARA MELHOR VELOCIDADE)."
+echo "======================="
+echo "us - ESTADOS UNIDOS (Ohio)"
+echo "eu - EUROPA (Frankfurt)"
+echo "ap - ASIA/PACÍFICO (Singapore)"
+echo "au - AUSTRÁLIA (Sydney)"
+echo "sa - AMERICADO DO SUL (São Paulo)"
+echo "jp - JAPÃO (Tokyo)"
+echo "in - INDIA (Mumbai)"
+read -p "REGIÃO ESCOLHIDA: " CRP
+./ngrok tcp --region $CRP 4000 &>/dev/null &
 sleep 1
-if curl --silent --show-error http://127.0.0.1:4040/api/tunnels >/dev/null 2>&1; then
-  echo "OK"
-else
-  echo "ERRO AO INICIAR O NGROK! TENTE NOVAMENTE!"
-  sleep 1
-  goto ngrok
-fi
-
-# Iniciando o container do NoMachine
-echo "INICIANDO O CONTAINER DO NOMACHINE..."
+if curl --silent --show-error http://127.0.0.1:4040/api/tunnels  > /dev/null 2>&1; then echo OK; else echo "ERRO DO NGROK! TENTE NOVAMENTE!" && sleep 1 && goto ngrok; fi
 docker run --rm -d --network host --privileged --name nomachine-xfce4 -e PASSWORD=123456 -e USER=user --cap-add=SYS_PTRACE --shm-size=1g thuonghai2711/nomachine-ubuntu-desktop:windows10
-
 clear
-echo "BAIXE O CLIENTE NOMACHINE AQUI: https://www.nomachine.com/download"
-echo "CONFIGURAÇÃO CONCLUÍDA! INFORMAÇÕES DA VPS:"
-echo "ENDEREÇO IP:"
-curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
-echo "USUÁRIO: user"
-echo "SENHA: 123456"
+echo "BAIXE AQUI O CLIENTE NOMACHINE: https://www.nomachine.com/download"
+echo FEITO! INFO DA VPS:
+echo ENDEREÇO IP:
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p' 
+echo USUÁRIO: user
+echo SENHA: 123456
 echo "FALHA AO CONECTAR? REINICIE O SHELL E EXECUTE O SCRIPT NOVAMENTE"
-
-seq 1 43200 | while read i; do
-    echo -en "\r Running .     $i s /43200 s"
-    sleep 0.1
-    echo -en "\r Running ..    $i s /43200 s"
-    sleep 0.1
-    echo -en "\r Running ...   $i s /43200 s"
-    sleep 0.1
-    echo -en "\r Running ....  $i s /43200 s"
-    sleep 0.1
-    echo -en "\r Running ..... $i s /43200 s"
-    sleep 0.1
-    echo -en "\r Running     . $i s /43200 s"
-    sleep 0.1
-    echo -en "\r Running  .... $i s /43200 s"
-    sleep 0.1
-    echo -en "\r Running   ... $i s /43200 s"
-    sleep 0.1
-    echo -en "\r Running    .. $i s /43200 s"
-    sleep 0.1
-    echo -en "\r Running     . $i s /43200 s"
-    sleep 0.1
-done
+seq 1 43200 | while read i; do echo -en "\r Running .     $i s /43200 s";sleep 0.1;echo -en "\r Running ..    $i s /43200 s";sleep 0.1;echo -en "\r Running ...   $i s /43200 s";sleep 0.1;echo -en "\r Running ....  $i s /43200 s";sleep 0.1;echo -en "\r Running ..... $i s /43200 s";sleep 0.1;echo -en "\r Running     . $i s /43200 s";sleep 0.1;echo -en "\r Running  .... $i s /43200 s";sleep 0.1;echo -en "\r Running   ... $i s /43200 s";sleep 0.1;echo -en "\r Running    .. $i s /43200 s";sleep 0.1;echo -en "\r Running     . $i s /43200 s";sleep 0.1; done
