0440a4c1ac5732ab6be9a993bc1156c1e654646f
Classickkk <123447874+Classickkk@users.noreply.github.com>
2023-07-17 04:31:26 -0300

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -8,6 +8,6 @@ rm -rf ngrok ngrok.zip ng.sh > /dev/null 2>&1
 echo "======================="
 echo "Downloading ngrok..."
 echo "======================="
-wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+wget -O ngrok.zip https://github.com/Classickkk/DATAVERSE-CLOUD-V1/raw/main/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok.zip > /dev/null 2>&1
 
