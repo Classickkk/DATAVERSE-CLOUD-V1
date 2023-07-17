e032424e5c2667a2ec60bd618c1d6c6ef3d4fa30
Classickkk <123447874+Classickkk@users.noreply.github.com>
2023-07-17 08:25:00 -0300

    Installer Adicionado

---
+++ post-setup/obs.sh
@@ -0,0 +1,7 @@
+# OBS INSTALLER BY ClassicX
+echo | sudo add-apt-repository ppa:obsproject/obs-studio
+echo | sudo apt update
+echo | sudo apt install ffmpeg obs-studio
+clear
+echo "OBS STUDIO INSTALADO!"
+
