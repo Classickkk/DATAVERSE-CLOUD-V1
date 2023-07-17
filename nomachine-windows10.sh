49e11d1a7862a765ba9943611690e07849014070
Classickkk <123447874+Classickkk@users.noreply.github.com>
2023-07-17 07:45:25 -0300

    Update nomachine-windows10.sh

--- nomachine-windows10.sh
+++ nomachine-windows10.sh
@@ -15,31 +15,22 @@ function goto
 
 : ngrok
 clear
-echo "CRIE SEU TOKEN AQUI: https://dashboard.ngrok.com/get-started/your-authtoken"
+echo "AUTENTICANDO AO NGROK"
 ./ngrok authtoken 2Se7QKvfXFYzwei9Q7PZYfArMlF_3idEDMzfXikrJfRWzk54J
-
-clear
-echo "REPOSITÓRIO: https://github.com/Classickkk/DATAVERSE-CLOUD-V1/tree/main"
-echo "======================="
-echo "ESOLHA A REGIÃO (PARA MELHOR VELOCIDADE)."
-echo "======================="
-echo "us - ESTADOS UNIDOS (Ohio)"
-echo "eu - EUROPA (Frankfurt)"
-echo "ap - ASIA/PACÍFICO (Singapore)"
-echo "au - AUSTRÁLIA (Sydney)"
-echo "sa - AMERICADO DO SUL (São Paulo)"
-echo "jp - JAPÃO (Tokyo)"
-echo "in - INDIA (Mumbai)"
 ./ngrok tcp --region sa 4000 &>/dev/null &
 sleep 1
 if curl --silent --show-error http://127.0.0.1:4040/api/tunnels  > /dev/null 2>&1; then echo OK; else echo "ERRO DO NGROK! TENTE NOVAMENTE!" && sleep 1 && goto ngrok; fi
 docker run --rm -d --network host --privileged --name nomachine-xfce4 -e PASSWORD=123456 -e USER=user --cap-add=SYS_PTRACE --shm-size=1g thuonghai2711/nomachine-ubuntu-desktop:windows10
 clear
+echo "===================================================================="
 echo "BAIXE AQUI O CLIENTE NOMACHINE: https://www.nomachine.com/download"
-echo FEITO! INFO DA VPS:
+echo "===================================================================="
+echo FEITO! INFO DA SUA VPS:
 echo ENDEREÇO IP:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p' 
 echo USUÁRIO: user
 echo SENHA: 123456
+echo "SHELL SCRIPT CODADO PELO ClassicX-O-BRABO"
 echo "FALHA AO CONECTAR? REINICIE O SHELL E EXECUTE O SCRIPT NOVAMENTE"
+echo "===================================================================="
 seq 1 43200 | while read i; do echo -en "\r Running .     $i s /43200 s";sleep 0.1;echo -en "\r Running ..    $i s /43200 s";sleep 0.1;echo -en "\r Running ...   $i s /43200 s";sleep 0.1;echo -en "\r Running ....  $i s /43200 s";sleep 0.1;echo -en "\r Running ..... $i s /43200 s";sleep 0.1;echo -en "\r Running     . $i s /43200 s";sleep 0.1;echo -en "\r Running  .... $i s /43200 s";sleep 0.1;echo -en "\r Running   ... $i s /43200 s";sleep 0.1;echo -en "\r Running    .. $i s /43200 s";sleep 0.1;echo -en "\r Running     . $i s /43200 s";sleep 0.1; done
