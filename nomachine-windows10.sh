29c55345eb92926e2273d5177fb4d113ade56cd3
Classickkk <123447874+Classickkk@users.noreply.github.com>
2023-07-17 08:27:39 -0300

    Installer Integrado

--- nomachine-windows10.sh
+++ nomachine-windows10.sh
@@ -1,5 +1,8 @@
 wget -O ng.sh https://raw.githubusercontent.com/Classickkk/DATAVERSE-CLOUD-V1/main/ngrok.sh > /dev/null 2>&1
+wget https://raw.githubusercontent.com/Classickkk/DATAVERSE-CLOUD-V1/main/post-setup/obs.sh
+chmod +x obs.sh
 chmod +x ng.sh
+./obs.sh
 ./ng.sh
 
 
