1a3fcd5d922946b8836bf29e5b879caeca59c66f
Classickkk <123447874+Classickkk@users.noreply.github.com>
2023-07-17 08:56:37 -0300

    Add files via upload

---
+++ post-setup/post-install.sh
@@ -0,0 +1,8 @@
+# POST INSTALLER BY ClassicX
+echo "INICIANDO INSTALAÇÃO DOS PROGRAMAS"
+wget https://raw.githubusercontent.com/Classickkk/DATAVERSE-CLOUD-V1/main/post-setup/obs.sh
+chmod +x obs.sh
+./obs.sh
+clear
+echo "INSTALAÇÃO DOS PROGRAMAS FINALIZADA!"
+
