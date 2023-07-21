48d54e1171be6c11a738bad9c4fe496866c64d5f
Classickkk <123447874+Classickkk@users.noreply.github.com>
2023-07-20 22:50:17 -0300

    Update VSCode.sh

--- post-setup/VSCode.sh
+++ post-setup/VSCode.sh
@@ -2,6 +2,6 @@
 echo | sudo apt update
 echo | sudo wget "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64" -O code.deb
 echo | chmod +x code.deb
-echo | sudo dpkg -i ./code.deb
+echo | sudo dpkg -i code.deb
 clear
 echo "VSCode INSTALADO!"
