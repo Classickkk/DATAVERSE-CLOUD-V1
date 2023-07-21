144f7f6c73a94a908b6bcc90518faefcdcc9babc
Classickkk <123447874+Classickkk@users.noreply.github.com>
2023-07-21 15:28:24 -0300

    Update post-install.sh

--- post-setup/post-install.sh
+++ post-setup/post-install.sh
@@ -1,15 +1,25 @@
 # POST INSTALLER BY ClassicX
-echo "INSTALANDO OS PROGRAMAS"
+echo "BAIXANDO ARQUIVOS"
 wget https://raw.githubusercontent.com/Classickkk/DATAVERSE-CLOUD-V1/main/post-setup/obs.sh
-chmod +x obs.sh
-./obs.sh
-clear
 wget https://raw.githubusercontent.com/Classickkk/DATAVERSE-CLOUD-V1/main/post-setup/filezilla.sh
-chmod +x filezilla.sh
-./filezilla.sh
 wget https://raw.githubusercontent.com/Classickkk/DATAVERSE-CLOUD-V1/main/post-setup/VSCode.sh
-chmod +x VSCode.sh
-./VSCode.sh
-clear
-echo "TODOS OS PROGRAMAS INSTALADOS COM SUCESSO!"
 
+echo "APLICANDO PERMISSÕES DE EXECUÇÃO NOS ARQUIVOS BAIXADOS"
+chmod +x obs.sh
+chmod +x filezilla.sh
+chmod +x VSCode.sh
+
+echo "DESEJA INSTALAR OS PROGRAMAS? (Y/N)"
+read resposta
+
+if [[ $resposta == "Y" || $resposta == "y" ]]; then
+    echo "INSTALANDO OS PROGRAMAS"
+    ./obs.sh
+    clear
+    ./filezilla.sh
+    ./VSCode.sh
+    clear
+    echo "TODOS OS PROGRAMAS INSTALADOS COM SUCESSO!"
+else
+    echo "INSTALAÇÃO CANCELADA"
+fi
